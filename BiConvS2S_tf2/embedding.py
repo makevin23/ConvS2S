ceda95b5998bf5e53ba0c2bf415d07c1d17726dd
Mengshi Ma <makevin23@gmail.com>
2022-03-21 12:19:38 +0100

    added comment for symbolic tensor, edited embedd

--- BiConvS2S_tf2/embedding.py
+++ BiConvS2S_tf2/embedding.py
@@ -19,12 +19,12 @@ class Embed():
             position_vectors.append(np.asarray(current_vector))
         self.position_vectors = np.asarray(position_vectors)
 
-    def generate_input_pairs(self, X):
+    def generate_input_pairs(self, training_sentences):
        
-        X = []
-        Y = []
+        words = []
+        previous_and_following_words = []
 
-        for sentence in X:
+        for sentence in training_sentences:
 
             # For each sentence in the training set
             for i in range(sentence.size):
@@ -33,23 +33,18 @@ class Embed():
                 # Append the word before it and the 
                 # word after it to the label (If either exist)
                 if(i + 1 != len(sentence)):
-                    X.append(sentence[i]) 
-                    Y.append(sentence[i + 1])
+                    words.append(sentence[i]) 
+                    previous_and_following_words.append(sentence[i + 1])
                 if(i - 1 != -1):
-                    X.append(sentence[i])
-                    Y.append(sentence[i - 1])
+                    words.append(sentence[i])
+                    previous_and_following_words.append(sentence[i - 1])
 
         # Convert them into numpy arrays
-        self.X = np.asarray(X)
-        self.Y = np.asarray(Y)
+        self.X = np.asarray(words)
+        self.Y = np.asarray(previous_and_following_words)
         self.Y = np.expand_dims(self.Y, 1)
     
-    def one_hot_encoding(self, X):
-        one_hot = np.zeros((X.shape[0], X.shape[1], self.vocab_size))
-        for i in range(X.shape[0]):
-            for j in range(X.shape[1]):
-                one_hot[i, j, X[i, j]] = 1
-        return one_hot
+
 
     def train_embedder(self, X):
 
@@ -65,9 +60,19 @@ class Embed():
 
 
     def get_embedding(self, X):
-        word_embeddings = []
-
+        embedded_words = []
+        
         return 1
 
-    def generate_embeddings(self, X):
-        return (self.get_embedding(X) + self.get_position_embedding(X))
+    def generate_embeddings(self, training_sentences):
+        return (self.get_embedding(training_sentences) + self.get_position_embedding(training_sentences))
+
+
+
+
+    def one_hot_encoding(self, X):
+        one_hot = np.zeros((X.shape[0], X.shape[1], self.vocab_size))
+        for i in range(X.shape[0]):
+            for j in range(X.shape[1]):
+                one_hot[i, j, X[i, j]] = 1
+        return one_hot
