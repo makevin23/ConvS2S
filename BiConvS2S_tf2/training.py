a094c45a4d68a30fc4c98993fe3915c6796f73e4
Mengshi Ma <makevin23@gmail.com>
2022-03-31 12:17:49 +0200

    changed session with tf.function

--- BiConvS2S_tf2/training.py
+++ BiConvS2S_tf2/training.py
@@ -1,3 +1,4 @@
+from json import decoder
 import tensorflow as tf
 import numpy as np
 import os
@@ -16,8 +17,9 @@ class Translator():
         self.batch_size = batch_size
         self.epochs = epochs
         self.lr = learning_rate
-        self.target_pl = tf.compat.v1.placeholder(dtype = tf.float32, shape = [None, self.decoder.max_length - 1, self.vocab_size], name = "Forward_targets")
-        self.target_pl_rev = tf.compat.v1.placeholder(dtype = tf.float32, shape = [None, self.encoder.max_length - 1, self.vocab_size], name = "Reverse_targets")
+        
+        # self.target_pl = tf.compat.v1.placeholder(dtype = tf.float32, shape = [None, self.decoder.max_length - 1, self.vocab_size], name = "Forward_targets")
+        # self.target_pl_rev = tf.compat.v1.placeholder(dtype = tf.float32, shape = [None, self.encoder.max_length - 1, self.vocab_size], name = "Reverse_targets")
 
 # The __call__() enables use to build a callable object.
    
@@ -38,6 +40,33 @@ class Translator():
             output = self.start_eval(inputs)
             return output
 
+
+    def init_train_step(self, input_x, decoder_input, target, encoder_input_rev, input_x_rev, target_rev):
+        self.encoder.X = input_x
+        self.decoder.input_x = decoder_input
+        self.target_pl = target
+        self.encoder.X_rev = encoder_input_rev
+        self.decoder.input_x_rev = input_x_rev
+        self.target_pl_rev = target_rev
+
+    @tf.function
+    def train_step_for(self):
+        optimizer = tf.keras.optimizer.Adam(learning_rate = self.lr)
+        encoder_output, encoder_attention = self.encoder.for_encoder()
+        prob_output = self.decoder.for_decoder(encoder_output, encoder_attention)
+        loss_fxn_for = tf.reduce_mean(self.loss(labels = self.target_pl, logits = prob_output))
+        optimizer.minimize(loss_fxn_for)
+        return loss_fxn_for
+
+    @tf.function
+    def train_step_rev(self):
+        optimizer = tf.compat.v1.train.AdamOptimizer(learning_rate = self.lr)
+        decoder_output, decoder_attention = self.decoder.rev_decoder()
+        prob_output_rev = self.encoder.rev_encoder(decoder_output, decoder_attention)
+        loss_fxn_rev = tf.reduce_mean(self.loss(labels = self.target_pl_rev, logits = prob_output_rev))
+        optimizer.minimize(loss_fxn_rev)
+        return loss_fxn_rev
+
 # Training the model:
     def start_train(self, inputs, targets):
 
@@ -57,90 +86,72 @@ class Translator():
         train_embeddings = self.embedder.generate_embeddings(inputs)
         label_embeddings = self.embedder.generate_embeddings(targets)
 
-        # Start the Tensorflow Session
-        with tf.compat.v1.Session() as sess:
 
-            # Define the network, the optimizer and the loss function
-            loss = []
-            optimizer = tf.compat.v1.train.AdamOptimizer(learning_rate = self.lr)
-            encoder_output, encoder_attention = self.encoder.for_encoder()
-            prob_output = self.decoder.for_decoder(encoder_output, encoder_attention)
-            loss_fxn_for = tf.reduce_mean(self.loss(labels = self.target_pl, logits = prob_output))
-            min_op_for = optimizer.minimize(loss_fxn_for)
-            decoder_output, decoder_attention = self.decoder.rev_decoder()
-            prob_output_rev = self.encoder.rev_encoder(decoder_output, decoder_attention)
-            loss_fxn_rev = tf.reduce_mean(self.loss(labels = self.target_pl_rev, logits = prob_output_rev))
-            min_op_rev = optimizer.minimize(loss_fxn_rev)
+        # Define the network, the optimizer and the loss function
+        loss = []
+        
+        # Start training for the number of epochs given
+        for i in range(self.epochs):
+            loss_epoch = []
+            for batch in index:
 
-            # Writer for the tensorflow graph. It also shows the loss function
-            writer = tf.compat.v1.summary.FileWriter(os.getcwd() + '/TensorBoard_' + str(np.random.randint(0, 10000)))
-            sess.run(tf.compat.v1.global_variables_initializer())
-            tf.summary.scalar('loss_for', tf.reduce_mean(loss_fxn_for))
-            tf.summary.scalar('loss_rev', tf.reduce_mean(loss_fxn_rev))
-            merger = tf.compat.v1.summary.merge_all()
-            writer.add_graph(sess.graph)
+                # Generate the inputs and the targets from the embeddings generated
+                input_x = train_embeddings[batch, :, :]
+                decoder_input = label_embeddings[batch, :-1, :]
+                input_x_rev = label_embeddings[batch, :, :]
+                encoder_input_rev = train_embeddings[batch, :-1, :]
 
-            # Start training for the number of epochs given
-            for i in range(self.epochs):
-                loss_epoch = []
-                for batch in index:
+                # One Hot encode the targets for loss calculations
+                target = self.embedder.one_hot_encoding(targets[batch, 1:])
+                target_rev = self.embedder.one_hot_encoding(inputs[batch, 1:])
 
-                    # Generate the inputs and the targets from the embeddings generated
-                    input_x = train_embeddings[batch, :, :]
-                    decoder_input = label_embeddings[batch, :-1, :]
-                    input_x_rev = label_embeddings[batch, :, :]
-                    encoder_input_rev = train_embeddings[batch, :-1, :]
+                # Run the minimization and the loss function op
+                self.init_train_step(input_x, decoder_input, target, encoder_input_rev, input_x_rev, target_rev)
+                loss_val_for = self.train_step_for()
+                loss_val_rev =  self.train_step_rev()
 
-                    # One Hot encode the targets for loss calculations
-                    target = self.embedder.one_hot_encoding(targets[batch, 1:])
-                    target_rev = self.embedder.one_hot_encoding(inputs[batch, 1:])
+            
+                # Run the summary merger op and add the summary to the tensorboard tfevents file
+                # summ = sess.run(merger, feed_dict = { self.encoder.X : input_x, self.decoder.input_x : decoder_input, self.target_pl : target, self.encoder.X_rev : encoder_input_rev, self.decoder.input_x_rev : input_x_rev, self.target_pl_rev : target_rev})
+                # writer.add_summary(summ, n_batches*i + sum(batch))
+                loss_epoch.append([loss_val_for, loss_val_rev])
+            loss.append(np.mean(loss_epoch, axis = 0))
 
-                    # Run the minimization and the loss function op
-                    _, loss_val_for = sess.run([min_op_for, loss_fxn_for], 
-                    feed_dict = { self.encoder.X : input_x, self.decoder.input_x : decoder_input, self.target_pl : target, 
-                    self.encoder.X_rev : encoder_input_rev, self.decoder.input_x_rev : input_x_rev, self.target_pl_rev : target_rev})
-                    _, loss_val_rev = sess.run([min_op_rev, loss_fxn_for], 
-                    feed_dict = { self.encoder.X : input_x, self.decoder.input_x : decoder_input, self.target_pl : target, 
-                    self.encoder.X_rev : encoder_input_rev, self.decoder.input_x_rev : input_x_rev, self.target_pl_rev : target_rev})
+            # Print the average loss after each epoch
+            print("EPOCH " + str(i + 1) + " COMPLETED !")
+            print("LOSS : " + str(loss[i]))
 
-                    # Run the summary merger op and add the summary to the tensorboard tfevents file
-                    # summ = sess.run(merger, feed_dict = { self.encoder.X : input_x, self.decoder.input_x : decoder_input, self.target_pl : target, self.encoder.X_rev : encoder_input_rev, self.decoder.input_x_rev : input_x_rev, self.target_pl_rev : target_rev})
-                    # writer.add_summary(summ, n_batches*i + sum(batch))
-                    loss_epoch.append([loss_val_for, loss_val_rev])
-                loss.append(np.mean(loss_epoch, axis = 0))
-
-                # Print the average loss after each epoch
-                print("EPOCH " + str(i + 1) + " COMPLETED !")
-                print("LOSS : " + str(loss[i]))
-            sess.close()
         print("Finished Optimization")
+
+
+    @tf.function
+    def output_step(self, encoder_X, decoder_input_x):
+        self.encoder.X = encoder_X
+        self.decoder.input_x = decoder_input_x
+        encoder_output, encoder_attention = self.encoder.for_encoder()
+        return self.decoder.for_decoder(encoder_output, encoder_attention)
+
 
 # Run the network for the given inputs
     def start_eval(self, inputs):
-        with tf.compat.v1.Session() as sess:
 
-            # Build the network
-            encoder_output, encoder_attention = self.encoder.for_encoder()
-            prob_output = self.decoder.for_decoder(encoder_output, encoder_attention)
-            decoder_inputs = np.zeros((inputs.shape[0], self.decoder.max_length - 1))
-            decoder_inputs[:, 0] = np.ones((inputs.shape[0]))
-            next_decoder_output = None
-            i = 1
+        decoder_inputs = np.zeros((inputs.shape[0], self.decoder.max_length - 1))
+        decoder_inputs[:, 0] = np.ones((inputs.shape[0]))
+        next_decoder_output = None
+        i = 1
 
             # Start the session. While the output_length is less than the max_length for the decoder, run the netwrok
-            sess.run(tf.compat.v1.global_variables_initializer())
-            while(next_decoder_output is None or next_decoder_output[0, 0] is not 1) and len(decoder_inputs[0, :]) < self.decoder.max_length and i < self.decoder.max_length - 1:
-                output = sess.run(prob_output, feed_dict = {
-                    self.encoder.X : self.embedder.generate_embeddings(inputs), 
-                    self.decoder.input_x : self.embedder.generate_embeddings(decoder_inputs)
-                })
+        while(next_decoder_output is None or next_decoder_output[0, 0] is not 1) and len(decoder_inputs[0, :]) < self.decoder.max_length and i < self.decoder.max_length - 1:
 
-                # Convert the output into vocab indices
-                next_decoder_output = np.argmax(output, axis = 2)
+            output = self.output_step(self.embedder.generate_embeddings(inputs), self.embedder.generate_embeddings(decoder_inputs))
 
-                # Update the decoder inputs to reflect this prediction.
-                decoder_inputs[:, i] = next_decoder_output[:, i]
-                i += 1
+            # Convert the output into vocab indices
+            next_decoder_output = np.argmax(output, axis = 2)
+
+            # Update the decoder inputs to reflect this prediction.
+            decoder_inputs[:, i] = next_decoder_output[:, i]
+            i += 1
+
         output_strings = []
 
         # Convert the index vectors into words and subsequently, sentences
