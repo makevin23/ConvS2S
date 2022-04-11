trying to fix no gradients

BiConvS2S_tf2/algo_test.py
@@ -6,9 +6,7 @@
 
 import os
 import pickle
-import tensorflow as tf
 import numpy as np
-import string
 import embedding
 import conv_encoder
 import conv_decoder
@@ -71,8 +69,8 @@ data_dir = 'BiConvS2S_tf2/data'
 # examples = load_data(data_dir+'/data.en')
 # target = load_data(data_dir+'/data.sparql')
 
-print(examples[:3])
-print(target[:3])
+# print(examples[:3])
+# print(target[:3])
 
 trained_model_dir = "BiConvS2S_tf2"
 ckpt_dir = os.path.join(trained_model_dir, "ckpt")
@@ -152,31 +150,33 @@ for sentence in target:
     train_y.append(vectorized)
 
 # Pad the examples and the labels with zero to ensure equal length
-print("Max input length: ", max_input_length)
-print("Max target length: ", max_target_length)
+# print("Max input length: ", max_input_length)
+# print("Max target length: ", max_target_length)
 train_x = np.asarray([np.pad(example, [0, max_input_length - len(example) + 2], mode = 'constant') for example in train_x]).astype(int)
 train_y = np.asarray([np.pad(example, [0, max_target_length - len(example) + 2], mode = 'constant') for example in train_y]).astype(int)
 
-print(train_x[:3])
-print(train_y[:3])
+# print(train_x[:3])
+# print(train_y[:3])
 
-os.mkdir(pkl_dir)
-with open(pkl_dir+'/word_to_index.pkl', 'wb') as f:
-    pickle.dump(word_to_index, f)
-
-with open(pkl_dir+'/index_to_word.pkl', 'wb') as f:
-    pickle.dump(index_to_word, f)
-
-with open(pkl_dir+'/max_input_length.pkl', 'wb') as f:
-    pickle.dump(max_input_length, f)
-
-with open(pkl_dir+'/max_target_length.pkl', 'wb') as f:
-    pickle.dump(max_target_length, f)
+# store the vocabulary
+# os.mkdir(pkl_dir)
+# with open(pkl_dir+'/word_to_index.pkl', 'wb') as f:
+#     pickle.dump(word_to_index, f)
+# 
+# with open(pkl_dir+'/index_to_word.pkl', 'wb') as f:
+#     pickle.dump(index_to_word, f)
+# 
+# with open(pkl_dir+'/max_input_length.pkl', 'wb') as f:
+#     pickle.dump(max_input_length, f)
+# 
+# with open(pkl_dir+'/max_target_length.pkl', 'wb') as f:
+#     pickle.dump(max_target_length, f)
 
 # Train the Embedder Network on the examples
 embedder = embedding.Embed(word_to_index, 512, 16)
 embedder.train_embedder(train_x)
-np.save(pkl_dir+'/embedding_words.npy', embedder.embedding_words)
+# store the embedder
+# np.save(pkl_dir+'/embedding_words.npy', embedder.embedding_words)
 # train_embeddings = embedder.generate_embeddings(train_x)
 # label_embeddings = embedder.generate_embeddings(train_y)
 

BiConvS2S_tf2/conv_decoder.py
@@ -50,9 +50,8 @@ class ConvDecoder(tf.keras.Model):
         self.dense_layer_1 = tf.keras.layers.Dense(self.hidden_size, activation="relu", name = "decoder_dense_layer_1")
         self.dense_layer_2 = tf.keras.layers.Dense(self.embedding_size, activation="relu", name = "decoder_dense_layer_2")
         self.dense_layer_3 = tf.keras.layers.Dense(self.vocab_size, activation="relu", name = "decoder_dense_layer_3")
-        # TODO: replace Dense with Conv1D
-        self.layer_conv_embedding = tf.keras.layers.Dense(self.embedding_size, activation="relu", name = "decoder_conv_embedding")
-        self.layer_embedding_conv = tf.keras.layers.Dense(self.hidden_size, activation="relu", name = "decoder_embedding_conv")
+        self.layer_conv_embedding = tf.keras.layers.Dense(self.embedding_size, activation=tf.keras.activations.softmax, name = "decoder_conv_embedding")
+        self.layer_embedding_conv = tf.keras.layers.Dense(self.hidden_size, activation=tf.keras.activations.softmax, name = "decoder_embedding_conv")
         self.conv_layer = tf.keras.layers.Conv2D(filters=2 * self.hidden_size, kernel_size=self.kernel_size, padding="same", name="decoder_conv_layer")
 
     def for_decoder(self, encoder_outputs, encoder_attention):

BiConvS2S_tf2/conv_encoder.py
@@ -36,9 +36,8 @@ class ConvEncoder(tf.keras.Model):
         self.dense_layer_1 = tf.keras.layers.Dense(self.hidden_size, activation="relu", name="encoder_dense_layer_1")
         self.dense_layer_2 = tf.keras.layers.Dense(self.embedding_size, activation="relu", name="encoder_dense_layer_2")
         self.dense_layer_3 = tf.keras.layers.Dense(self.vocab_size, activation="relu", name="encoder_dense_layer_3")
-        # TODO: replace dense layer with convolutional layer
-        self.layer_conv_embedding = tf.keras.layers.Dense(self.embedding_size, activation="relu", name="encoder_dense_layer_embedding")
-        self.layer_embedding_conv = tf.keras.layers.Dense(self.hidden_size, activation="relu", name="encoder_dense_layer_conv")
+        self.layer_conv_embedding = tf.keras.layers.Dense(self.embedding_size, activation=tf.keras.activations.softmax, name="encoder_conv_embedding")
+        self.layer_embedding_conv = tf.keras.layers.Dense(self.hidden_size, activation=tf.keras.activations.softmax, name="encoder_embedding_conv")
         self.conv_layer = tf.keras.layers.Conv2D(filters=2 * self.hidden_size, kernel_size=self.kernel_size, padding="same", name="encoder_conv_layer")
         
         # self.dense_layer_1 = tf.Variable(tf.random.truncated_normal([self.embedding_size, self.hidden_size], mean = 0, stddev = 1/np.sqrt(self.embedding_size)), name = "Layer_1_Encoder")

BiConvS2S_tf2/training.py
@@ -51,20 +51,20 @@ class Translator():
         self.decoder.input_x_rev = input_x_rev
         self.target_pl_rev = target_rev
 
-    # @tf.function
     def train_step_for(self):
         with tf.GradientTape() as tape:
             encoder_output, encoder_attention = self.encoder.for_encoder()
             prob_output = self.decoder.for_decoder(encoder_output, encoder_attention)
             loss_fxn_for = tf.reduce_mean(self.loss(labels = self.target_pl, logits = prob_output))
-            # TODO: add var_list in minimize call, all layers in encoder?
-            # coder_layers = [self.encoder.dense_layer_1, self.encoder.dense_layer_2, self.encoder.dense_layer_3, self.encoder.layer_conv_embedding, self.encoder.layer_embedding_conv, self.encoder.conv_layer, 
-            #                 self.decoder.dense_layer_1, self.decoder.dense_layer_2, self.decoder.dense_layer_3, self.decoder.layer_conv_embedding, self.decoder.layer_embedding_conv, self.decoder.conv_layer]
+            print(self.encoder.trainable_weights)
+            # print(self.decoder.trainable_weights)
             variables = self.encoder.trainable_variables + self.decoder.trainable_variables
+            # TODO: fix gradients for dense_layer_3, conv_embedding, embedding_conv
             gradients = tape.gradient(loss_fxn_for, variables)
-            # print(variables)
-            # print(gradients)
-            self.optimizer.apply_gradients(zip(gradients, variables))
+            # print("variables: ", variables)
+            # print("gradients: ", gradients)
+            # self.optimizer.apply_gradients(zip(gradients, variables))
+            self.optimizer.apply_gradients((gradients, variables) for (gradients, variables) in zip(gradients, variables) if gradients is not None)
             # optimizer.minimize(loss_fxn_for, var_list=coder_layers, tape=tape)
         return loss_fxn_for
 